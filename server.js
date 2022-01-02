3b6366d710ec02ac43fd34837d24da73ed2e5046
Arnav Kumar <amkumar@princeton.edu>
2022-01-02 13:35:56 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sports-comparer'));
+app.use(express.static(__dirname + '/dist/SportsComparer'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/sports-comparer/index.html'));});
+    res.sendFile(path.join(__dirname+  '/dist/SportsComparer/index.html'));
+});
 app.listen(process.env.PORT || 8080);
